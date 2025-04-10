91bd1c5018dde260a6c9b13aea9e23804d5ffa07
manuelmv15 <mv23036@ues.edu.sv>
2025-04-09 22:07:49 -0600

    ejercicio de los triangulos de niveles variables

--- ejer01.js
+++ ejer01.js
@@ -1,18 +1,36 @@
+
+const DibujarTriangulo = function (niveles) {
+    let espacio = niveles - 1;
+    for (let index = 1; index <= niveles; index++) {
+
+        for (let j = 0; j < espacio; j++) {
+            process.stdout.write(" ");
+        }
+        for (let j = 0; j < index * 2 - 1; j++) {
+            process.stdout.write("*");
+        }
+        console.log(" ")
+        espacio--;
+    } 
+}
+
+console.log("Triangulo de 7 niveles")
+DibujarTriangulo(7)
+
 const prompt = require('prompt-sync')();
 
-let altura = 7;
+console.log("\n\n")
 
-let espacio = altura - 1;
-
-for (let index = 1; index <= altura; index++) {
-
-    for (let j = 0; j < espacio; j++) {
-        process.stdout.write(" ");
+do {
+    let altura = prompt("Ingrese la altura del triangulo: ")    
+    if (altura<3) {
+        console.log("La altura debe ser mayor/igual a 3")
     }
-    for (let j = 0; j < index * 2 - 1; j++) {
-        process.stdout.write("*");
+    else{
+        DibujarTriangulo(altura)
+        break;
     }
-    console.log(" ")
-    espacio--;
+} while (true);
 
-} 
+
+
