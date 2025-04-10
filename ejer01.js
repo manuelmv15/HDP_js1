a0790a2a500bde5feedc3dc864a8ea9746f161e6
manuelmv15 <mv23036@ues.edu.sv>
2025-04-09 21:56:53 -0600

    ejercicio de triangulo de 7 niveles

--- ejer01.js
+++ ejer01.js
@@ -1,1 +1,18 @@
+const prompt = require('prompt-sync')();
 
+let altura = 7;
+
+let espacio = altura - 1;
+
+for (let index = 1; index <= altura; index++) {
+
+    for (let j = 0; j < espacio; j++) {
+        process.stdout.write(" ");
+    }
+    for (let j = 0; j < index * 2 - 1; j++) {
+        process.stdout.write("*");
+    }
+    console.log(" ")
+    espacio--;
+
+} 
